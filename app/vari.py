048d636478baa997e507b9e277a6986412ad1ca5
Palmcm <palmcm@Palmcms-MacBook-Air.local>
2019-11-28 18:39:36 +0700

    Add C extension

--- app/vari.py
+++ app/vari.py
@@ -1,5 +1,6 @@
 from app import app
 from flask import request
+import json 
 var_dict = {}
 
 @app.route('/variable/<vari>', methods=['PUT'])
@@ -8,11 +9,35 @@ def put_var(vari):
     if vari in var_dict:
         status = 204
     var_dict[vari] = request.json['value']
-    return status
+    return "", status
 
 @app.route('/variable/<vari>', methods=['GET'])
 def get_var(vari):
     if vari in var_dict:
         return {"value": var_dict[vari]}, 200
     else:
-        return 404
+        return "", 404
+
+@app.route('/variable', methods=['GET'])
+def dump():
+        result = json.dumps(var_dict)
+        return {"data": result}, 200
+
+@app.route('/variable', methods=['POST'])
+def load():
+        body = request.json
+        data = body['data']
+        if 'overwrite' in body:
+            sover = body['overwrite']
+            if sover == 'true':
+                over = True
+            else:
+                over = False
+        else:
+            over = False
+        ndict = json.loads(data)
+        for vari in ndict:
+            if over or vari not in var_dict:
+                var_dict[vari] = ndict[vari]
+
+        return "",204
